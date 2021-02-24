ecbcc1d3d52778dd814de92b48ca01e60bfd869b
Arief Rachman <arpodungge@gmail.com>
2021-02-24 22:00:28 +0700

    add readme

---
+++ README.md
@@ -0,0 +1,34 @@
+**Waktu Sholat - Gnome Shell Extention**
+
+Versi sederhana dari [fahrinh azan-gnome-shell-extension](https://github.com/fahrinh/azan-gnome-shell-extension)
+
+Disesuaikan untuk wilayah Indonesia :
+- Subuh saat Matahari berada pada sudut -20° di bawah horizon Timur
+- Isya' saat Matahari berada pada sudut -18° di bawah horizon Barat
+
+**Requirement**
+gnome-shell versi 3.6
+
+**Install :**
+- copy folder waktu-sholat@arpodungge.github.com ke ~/.local/share/gnome-shell/extensions
+- Tekan alt-F2 lalu tekan r dan enter
+- Buka Extensions atau Gnone Tweak lalu aktifkan extension waktu-sholat
+
+**Catatan**
+Dalam versi ini tidak disertakan ui untuk konfigurasi
+untuk melakukan konfigurasi silahkan edit file extension.js
+
+// Konfigurasi
+// Default latlon Jakarta : -6.1333, 106.75
+this._myLocation = [-6.1333, 106.75, 10];  
+this._myTimezone = +7;
+this._timeFormat= '24h';
+this._prayerMethod='Indonesia';
+
+// Penyesuaian waktu sholat
+this._subuhTune=2;
+this._zuhurTune=2;
+this._asarTune=2;
+this._magribTune=2;
+this._isyaTune=2;
+
---
+++ waktu-sholat@arpodungge.github.com/extension.js
@@ -0,0 +1,234 @@
+const St = imports.gi.St;
+const Main = imports.ui.main;
+const Soup = imports.gi.Soup;
+const Lang = imports.lang;
+const Mainloop = imports.mainloop;
+const Clutter = imports.gi.Clutter;
+const PanelMenu = imports.ui.panelMenu;
+const PopupMenu = imports.ui.popupMenu;
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+
+const PrayTimes = Me.imports.PrayTimes;
+const HijriCalendarKuwaiti = Me.imports.HijriCalendarKuwaiti;
+
+const Sholat = new Lang.Class({
+    Name: 'Sholat',
+    Extends: PanelMenu.Button,
+
+    _init: function () {
+        this.parent(0.0, "Sholat", false);
+        this.indicatorText = new St.Label({
+            text: _("Sholat"),
+            y_align: Clutter.ActorAlign.CENTER
+        });
+        this.add_child(this.indicatorText);
+
+        // Konfigurasi
+        // Default latlon Jakarta : -6.1333, 106.75
+        this._myLocation = [-6.1333, 106.75, 10];  
+        this._myTimezone = +7;
+        this._timeFormat= '24h';
+        this._prayerMethod='Indonesia';
+
+        // Penyesuaian waktu sholat
+        this._subuhTune=2;
+        this._zuhurTune=2;
+        this._asarTune=2;
+        this._magribTune=2;
+        this._isyaTune=2;
+
+        this._dayNames = new Array("Ahad", "Senin", "Selasa", "Rabu", "Kamis", "Jum'at", "Sabtu");
+        this._monthNames = new Array("Muharram", "Shafar", "Rabi'ul Awwal", "Rabi'ul Akhir",
+            "Jumadil Awal", "Jumadil Akhir", "Rajab", "Sya'ban",
+            "Ramadhan", "Syawal", "Zulqaidah", "Zulhijjah");
+
+        this._timeNames = {
+            imsak: 'Imsak',
+            fajr: 'Subuh',
+            sunrise: 'Terbit',
+            dhuhr: 'Zuhur',
+            asr: 'Asar',
+            maghrib: 'Magrib',
+            isha: 'Isya',
+            midnight: 'Tengah Malam'
+        };
+
+        this._prayItems = {};
+
+        this._buildMenu();
+        this._refresh();
+    },    
+
+    _buildMenu: function() {
+
+        let dateMenuItem = new PopupMenu.PopupMenuItem(_(""), {
+            reactive: true, hover: false, activate: false
+        });
+        let bin = new St.Bin({
+            x_expand: true,
+            x_align: Clutter.ActorAlign.CENTER,
+        });
+
+        this._hijriLabel = new St.Label({text: 'Hijri Date'});
+        bin.add_actor(this._hijriLabel);
+        dateMenuItem.actor.add_actor(bin);
+    
+        this.menu.addMenuItem(dateMenuItem);
+        this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+
+        for (let prayerId in this._timeNames) {
+            let prayerName = this._timeNames[prayerId];
+            let prayMenuItem = new PopupMenu.PopupMenuItem(_(prayerName), {
+                reactive: true, hover: false, activate: false
+            });
+    
+            let bin = new St.Bin({
+                x_expand: true,
+                x_align: Clutter.ActorAlign.END,
+            });
+    
+            let prayLabel = new St.Label();
+            bin.add_actor(prayLabel);
+
+            prayMenuItem.actor.add_actor(bin);
+            this.menu.addMenuItem(prayMenuItem);
+
+            this._prayItems[prayerId] = {
+                menuItem: prayMenuItem,
+                label: prayLabel
+            };
+        }
+    },
+
+    _refresh: function () {
+        this._reloadPrayerTimes();
+        this._removeTimeout();
+        this._timeout = Mainloop.timeout_add_seconds(60, Lang.bind(this, this._refresh));
+        return true;
+    },
+
+    _reloadPrayerTimes: function() {
+        let currentDate = new Date();
+        let currentSeconds = this._calculateSecondsFromDate(currentDate);
+
+        let pt = PrayTimes.prayTimes;
+        pt.setMethod(this._prayerMethod);
+        pt.tune( {fajr: this._subuhTune, dhuhr: this._zuhurTune, asr: this._asarTune, maghrib: this._magribTune, isha: this._isyaTune} );
+
+        let timesStr = pt.getTimes(currentDate, this._myLocation, this._myTimezone, 'auto', this._timeFormat);
+        let timesFloat = pt.getTimes(currentDate, this._myLocation, this._myTimezone, 'auto', 'Float');
+
+        let nearestPrayerId;
+        let minDiffMinutes = Number.MAX_VALUE;
+        let isTimeForPraying = false;
+        for (let prayerId in this._timeNames) {
+            let prayerName = this._timeNames[prayerId];
+            let prayerTime = timesStr[prayerId];
+
+            this._prayItems[prayerId].label.text = prayerTime;
+
+            if (this._isPrayerTime(prayerId)) {
+
+                let prayerSeconds = this._calculateSecondsFromHour(timesFloat[prayerId]);
+
+                let ishaSeconds = this._calculateSecondsFromHour(timesFloat['isha']);
+                let fajrSeconds = this._calculateSecondsFromHour(timesFloat['fajr']);
+
+                if (prayerId === 'fajr' && currentSeconds > ishaSeconds) {
+                    prayerSeconds = fajrSeconds + (24 * 60 *60);
+                }
+
+                let diffSeconds = prayerSeconds - currentSeconds;
+
+                if (diffSeconds > 0) {
+                    let diffMinutes = ~~(diffSeconds / 60);
+
+                    if (diffMinutes == 0) {
+                        isTimeForPraying = true;
+                        nearestPrayerId = prayerId;
+                        break;
+                    } else if (diffMinutes <= minDiffMinutes) {
+                        minDiffMinutes = diffMinutes;
+                        nearestPrayerId = prayerId;
+                    }
+                }
+
+            }
+        };
+
+        let hijriDate = HijriCalendarKuwaiti.KuwaitiCalendar();
+        let outputIslamicDate = this._formatHijriDate(hijriDate);
+        this._hijriLabel.text = outputIslamicDate;
+
+        if (isTimeForPraying) {
+            Main.notify(_("Sudah masuk waktu sholat " + this._timeNames[nearestPrayerId]));
+            this.indicatorText.set_text(_("Sholat : " + this._timeNames[nearestPrayerId]));
+  
+        } else {
+            this.indicatorText.set_text(this._timeNames[nearestPrayerId] + '  -' + this._formatRemainingTimeFromMinutes(minDiffMinutes));
+        };
+
+    },
+
+    _calculateSecondsFromDate: function(date) {
+        return this._calculateSecondsFromHour(date.getHours()) + (date.getMinutes() * 60) + date.getSeconds();
+    },
+  
+    _calculateSecondsFromHour: function(hour) {
+        return (hour * 60 * 60);
+    },
+  
+    _isPrayerTime: function(prayerId) {
+        return prayerId === 'fajr' || prayerId === 'dhuhr' || prayerId === 'asr' || prayerId === 'maghrib' || prayerId === 'isha';
+    },
+  
+    _formatRemainingTimeFromMinutes: function(diffMinutes) {
+        // let diffMinutes = diffSeconds / (60);
+  
+        let hours = ~~(diffMinutes / 60);
+        let minutes = ~~(diffMinutes % 60);
+  
+        let hoursStr = (hours < 10 ? "0" : "") + hours;
+        let minutesStr = (minutes < 10 ? "0" : "") + minutes;
+  
+        return hoursStr + ":" + minutesStr;
+    },
+
+    _formatHijriDate: function(hijriDate) {
+        return this._dayNames[hijriDate[4]] + ", " + hijriDate[5] + " " + this._monthNames[hijriDate[6]] + " " + hijriDate[7] + " H";
+    },
+
+    _removeTimeout: function () {
+        if (this._timeout) {
+          Mainloop.source_remove(this._timeout);
+          this._timeout = null;
+        }
+      },
+    
+      stop: function () {
+        if (this._timeout)
+          Mainloop.source_remove(this._timeout);
+        this._timeout = undefined;
+    
+        this.menu.removeAll();
+      }
+
+});
+
+
+let menuSholat;
+
+function init() {
+    
+}
+
+function enable() {
+    menuSholat = new Sholat;
+	Main.panel.addToStatusArea('sholat-indicator', menuSholat);
+}
+
+function disable() {
+    menuSholat.stop();
+    menuSholat.destroy();
+}
